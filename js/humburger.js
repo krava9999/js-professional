611cc8f6ac5677dd966e70ec8f4972a7add78f1d
krava9999 <kosmo@internet.ru>
2021-06-01 12:52:25 +1000

    3 пункт дз

---
+++ js/humburger.js
@@ -0,0 +1,16 @@
+﻿class Hamburger {
+    constructor(size, stuffing) { 
+        this.size = size;
+        this.stuffing = stuffing;
+     }
+
+    //  render(){
+    //      return `<input type="radio" name="burger"> small
+    //              <input type="radio" name="burger" checked> big
+    //              `
+    //  }
+  }
+
+// const menu = new Hamburger();
+// let container = document.querySelector(".menu");
+// container.insertAdjacentHTML('beforeend', menu.render());
